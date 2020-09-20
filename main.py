dafd939ec746836cccce6e4e75a44d7b6569a15c
xxxwarrior <sklmasha@yandex.ru>
2020-09-20 12:40:33 +0300

    Delete unnecessary

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from app import create_app
-
-from flask import render_template
 from flask_admin import Admin
 
+from app import create_app
 from app.database import Post 
 from app.admin_views import AdminView, HomeAdminView
 
-
+# TODO pass config via terminal
 app = create_app()
-print(app.config['DB'])
 
 admin = Admin(app, 'FlaskApp', url='/', index_view=HomeAdminView(name='home'))
 admin.add_view(AdminView(Post, name='Posts'))
